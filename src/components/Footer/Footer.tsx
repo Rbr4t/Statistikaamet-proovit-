d21d108c2bac563e4a15ec6bb95ec48e41c50c99
Robert Koor <robertkoor0@gmail.com>
2026-03-12 22:16:15 +0200

    made the footer's content  stack when resized

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,3 @@
-import './Footer.css'
-
 function Footer() {
     return (
         <footer className="footer">
@@ -9,20 +7,17 @@ function Footer() {
                     <p>Robert Koor</p>
                     <p>Tartu Ülikool</p>
                 </div>
+                <div className="footer-divider" />
                 <div className="footer-section">
                     <h2>Statistikaamet kodutöö</h2>
                     <a href="https://github.com/rbr4t" target="_blank" rel="noopener noreferrer">
                         <p>Github</p>
                     </a>
-
                     <a href="https://www.linkedin.com/in/robert-koor-0b96463ab/" target="_blank" rel="noopener noreferrer">
                         <p>LinkedIn</p>
                     </a>
                 </div>
             </div>
-
         </footer>
     )
 }
-
-export default Footer
